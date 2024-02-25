50d89429b9216a6603498542f16f000193c44eae
vinitsyk <kibervolk55@gmail.com>
2024-02-25 02:06:32 -0300

    create a first controller

---
+++ API/Gorilla/controllers/GorillaController.cxx
@@ -0,0 +1,38 @@
+#include <string>
+#include <memory>
+#include <cstdlib>
+#include <jsoncpp/json/json.h>
+#include <drogon/HttpController.h>
+
+class Gorilla : public drogon::HttpController<Gorilla>
+{
+    public:
+
+        METHOD_LIST_BEGIN
+
+            ADD_METHOD_TO(Gorilla::Root, "/", {drogon::Get});
+
+        METHOD_LIST_END
+
+        void Root(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> && callback)
+        {
+            try 
+            {
+                Json::Value jsonContent;
+
+                jsonContent["Gorilla"] = drogon::utils::getUuid();
+
+                callback( drogon::HttpResponse::newHttpJsonResponse(jsonContent) );
+
+            }
+            catch ( const std::exception& error ) 
+            {
+                auto internal_error = drogon::HttpResponse::newHttpResponse();
+
+                internal_error->setStatusCode(drogon::k500InternalServerError);
+
+                callback(internal_error);
+            }
+        }
+
+};
